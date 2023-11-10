78a4e0b3cdf74942e24596a1189c1f3924948923
Carlos leon <karlos_eb1@hotmail.com>
2023-11-10 01:46:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,64 @@ const MockAdapter = require('@bot-whatsapp/database/mock')
  * Primero declaras los submenus 1.1 y 2.1, luego el 1 y 2 y al final el principal.
  */
 
+const flowMensaje = addKeyword('btn')
+    .addAnswer(
+        'Aqui va un mensaje',
+        {
+            capture: true
+        },
+        async (ctx, {provider}) => {
+            const list = {
+                "header": {
+                    "type": "text",
+                    "text": "<HEADER_TEXT>"
+                },
+                "body": {
+                    "text": "<BODY_TEXT>"
+                },
+                "footer": {
+                    "text": "<FOOTER_TEXT>"
+                },
+                "action": {
+                    "button": "<BUTTON_TEXT>",
+                    "sections": [
+                        {
+                            "title": "<LIST_SECTION_1_TITLE>",
+                            "rows": [
+                                {
+                                    "id": "<LIST_SECTION_1_ROW_1_ID>",
+                                    "title": "<SECTION_1_ROW_1_TITLE>",
+                                    "description": "<SECTION_1_ROW_1_DESC>"
+                                },
+                                {
+                                    "id": "<LIST_SECTION_1_ROW_2_ID>",
+                                    "title": "<SECTION_1_ROW_2_TITLE>",
+                                    "description": "<SECTION_1_ROW_2_DESC>"
+                                }
+                            ]
+                        },
+                        {
+                            "title": "<LIST_SECTION_2_TITLE>",
+                            "rows": [
+                                {
+                                    "id": "<LIST_SECTION_2_ROW_1_ID>",
+                                    "title": "<SECTION_2_ROW_1_TITLE>",
+                                    "description": "<SECTION_2_ROW_1_DESC>"
+                                },
+                                {
+                                    "id": "<LIST_SECTION_2_ROW_2_ID>",
+                                    "title": "<SECTION_2_ROW_2_TITLE>",
+                                    "description": "<SECTION_2_ROW_2_DESC>"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+            await provider.sendLists(ctx.from, list)
+        }
+    )
+
 const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
 
 const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
@@ -64,13 +122,14 @@ const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
     .addAnswer(
         [
             'te comparto los siguientes links de interes sobre el proyecto',
+            '👉 *btn* para ver botones',
             '👉 *doc* para ver la documentación',
             '👉 *gracias*  para ver la lista de videos',
             '👉 *discord* unirte al discord',
         ],
         null,
         null,
-        [flowDocs, flowGracias, flowTuto, flowDiscord]
+        [flowMensaje,flowDocs, flowGracias, flowTuto, flowDiscord]
     )
 
 const main = async () => {
