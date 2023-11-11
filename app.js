bf2bd5a869f4d5c28351ea57334da3b40ba70181
Carlos leon <karlos_eb1@hotmail.com>
2023-11-11 15:43:00 -0700

    DIALOG

--- app.js
+++ app.js
@@ -2,94 +2,13 @@ const { createBot, createProvider, createFlow, addKeyword } = require('@bot-what
 
 const TwilioProvider = require('@bot-whatsapp/provider/twilio')
 const MockAdapter = require('@bot-whatsapp/database/mock')
+const { createBotDialog } = require('@bot-whatsapp/contexts/dialogflowcx')
 
-/**
- * Aqui declaramos los flujos hijos, los flujos se declaran de atras para adelante, es decir que si tienes un flujo de este tipo:
- *
- *          Menu Principal
- *           - SubMenu 1
- *             - Submenu 1.1
- *           - Submenu 2
- *             - Submenu 2.1
- *
- * Primero declaras los submenus 1.1 y 2.1, luego el 1 y 2 y al final el principal.
- */
 
-const flowMensaje = addKeyword('btn')
-.addAnswer(
-    'Aqui va un mensaje',
-    {
-        capture: true,
-        buttons: [
-            {body: 'opcion 1'},
-            {body: 'opcion 2'},
-            {body: 'opcion 3'},
-        ]
-    },
-)
-
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
-
-const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
-    [
-        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
-        'https://bot-whatsapp.netlify.app/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
-    [
-        '🙌 Aquí encontras un ejemplo rapido',
-        'https://bot-whatsapp.netlify.app/docs/example/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
-    [
-        '🚀 Puedes aportar tu granito de arena a este proyecto',
-        '[*opencollective*] https://opencollective.com/bot-whatsapp',
-        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
-        '[*patreon*] https://www.patreon.com/leifermendez',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowDiscord = addKeyword(['discord']).addAnswer(
-    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
-    .addAnswer(
-        [
-            'te comparto los siguientes links de interes sobre el proyecto',
-            '👉 *btn* para ver botones',
-            '👉 *doc* para ver la documentación',
-            '👉 *gracias*  para ver la lista de videos',
-            '👉 *discord* unirte al discord',
-        ],
-        null,
-        null,
-        [flowMensaje,flowDocs, flowGracias, flowTuto, flowDiscord]
-    )
 
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+
 
     const adapterProvider = createProvider(TwilioProvider, {
         accountSid: 'AC7638649cfc7203b4811175ddfdcc1673',
@@ -97,11 +16,17 @@ const main = async () => {
         vendorNumber: '+14155238886',
     })
 
-    createBot({
-        flow: adapterFlow,
-        provider: adapterProvider,
-        database: adapterDB,
-    })
+    createBotDialog(
+        {
+
+            provider: adapterProvider,
+            database: adapterDB,
+        },
+        {
+            location: 'us-west1',
+            agentId: 'ec9018a2-92ad-4c33-8f7c-b060875b7b32',
+        }
+    )
 }
 
 main()
